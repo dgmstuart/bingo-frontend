2e2e350ed33bbb1d02e19ba9dd2617969b4ad1e0
Duncan Stuart <dgmstuart@gmail.com>
2022-02-06 03:33:29 +0100

    Display some words on the page
    Just getting started: display the first 25 words in the word list on the
    page - no random selection or order.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(document.documentElement).toHaveTextContent("Wedge formation");
+  expect(document.documentElement).toHaveTextContent("Circle formation");
 });
