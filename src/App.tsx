8f75bd1cf674754365d4a427992c4c4175c3d20f
Duncan Stuart <dgmstuart@gmail.com>
2022-02-06 06:12:22 +0100

    Display words in a grid
    Experimented with a few different ways of laying out the words.
    Concluded that table is maybe the best of a bad bunch.

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,41 @@ import wordList from './wordList.json'
 function App() {
   var words: string[] = wordList.slice(0, 25)
 
+  const Words: React.FC<{ words: string[] }> = ({ words }) => {
+    return(
+      <table className="Words">
+        <Row words={words.slice(0,5)}/>
+        <Row words={words.slice(5,10)}/>
+        <Row words={words.slice(10,15)}/>
+        <Row words={words.slice(15,20)}/>
+        <Row words={words.slice(20,25)}/>
+      </table>
+    )
+  }
+
+  const Row: React.FC<{ words: string[] }> = ({ words }) => {
+    return(
+      <tr className="Row">
+        {
+          words.map((word, index)=>
+            <Cell key={index} word={word}/>
+          )
+        }
+      </tr>
+    )
+  }
+
+  const Cell: React.FC<{ word: string }> = ({ word }) => {
+    return(<td className="Cell">{word}</td>)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Team Lindy Bingo</h1>
       </header>
 
-      {words.map((word, index)=><p key={index}>{word}</p>)}
+      <Words words={words}/>
     </div>
   );
 }
