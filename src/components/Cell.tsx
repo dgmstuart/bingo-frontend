bdd25d0268912c51369bfbc548bf8aaeea29aa0e
Duncan Stuart <dgmstuart@gmail.com>
2023-02-16 00:44:15 +0100

    Refactor: Extract normaliseWord
    We're going to need this in order to display the word list properly.

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Cell.css";
 import classNames from "classnames";
+import normaliseWord from "../lib/normaliseWord";
 import type { ClickHandler } from "../clickHandler";
 
 export type CellClickHandler = ClickHandler<HTMLTableDataCellElement>;
@@ -10,14 +11,9 @@ export type CellProps = CellData & { toggleStamped: CellClickHandler };
 const Cell: React.FC<CellProps> = ({ word, stamped, toggleStamped }) => {
   const classes = classNames("Cell", { stamped: stamped });
 
-  const htmlSoftHyphen = "&shy;";
-  const unicodeSoftHyphen = "\u00ad";
-  const normalise = (string: string): string =>
-    string.replace(new RegExp(htmlSoftHyphen, "g"), unicodeSoftHyphen);
-
   return (
     <td role="gridcell" className={classes} onClick={toggleStamped}>
-      {normalise(word)}
+      {normaliseWord(word)}
     </td>
   );
 };
