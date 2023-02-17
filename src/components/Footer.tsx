21d319f7497fe6b0f7632fadc24595ef9cd459df
Duncan Stuart <dgmstuart@gmail.com>
2023-02-17 17:19:32 +0100

    Add external links to the Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,22 @@ const Footer: React.FC = () => {
         <li>
           <Link to="/word_list">Full word list</Link>
         </li>
+        <li>
+          <a
+            href="https://www.youtube.com/playlist?list=PLgsIo5h4KQoYzMCvcuBFTy7-qW8VgsFpT"
+            className="external"
+          >
+            Full video list
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://dgmstuart.github.io/blog/2022/02/18/building-a-bingo-app-in-react/"
+            className="external"
+          >
+            About
+          </a>
+        </li>
       </ul>
     </footer>
   );
