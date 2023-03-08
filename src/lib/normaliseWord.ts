7d82c9f884212688fda9854286902cb1b72bb40b
Duncan Stuart <dgmstuart@gmail.com>
2023-03-08 01:50:02 +0100

    Updates to words after testing at Xin's

--- src/lib/normaliseWord.ts
+++ src/lib/normaliseWord.ts
@@ -1,6 +1,10 @@
 const htmlSoftHyphen = "&shy;";
 const unicodeSoftHyphen = "\u00ad";
+const htmlNonBreakingSpace = "&nbsp;";
+const unicodeNonBreakingSpace = "\u00a0";
 const normaliseWord = (string: string): string =>
-  string.replace(new RegExp(htmlSoftHyphen, "g"), unicodeSoftHyphen);
+  string
+    .replace(new RegExp(htmlSoftHyphen, "g"), unicodeSoftHyphen)
+    .replace(new RegExp(htmlNonBreakingSpace, "g"), unicodeNonBreakingSpace);
 
 export default normaliseWord;
