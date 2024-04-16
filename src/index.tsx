6e294c647494dc6a9e03d7bc698ed01568ccd1d8
Duncan Stuart <dgmstuart@gmail.com>
2024-04-16 23:50:05 +0200

    Migrate to Vite
    Followed this guide:
    
      https://medium.com/@aishwaryaparab1/deploying-vite-deploying-vite-app-to-github-pages-166fff40ffd3
    
    Note that apps no longer need a service worker to be considered
    installable:
    
    > "The goal of the existing install criteria is to incentivize
    > developers to invest in high quality user experiences and to meet
    > user
    > expectations when software is installed. However, the requirements in
    > the criteria didn't always produce that higher quality experience. For
    > example, the service worker check was meant as a proxy for detecting
    > sites with some offline experience, but sites added service workers
    > with empty fetch handlers to satisfy the criteria. This hurts web
    > performance instead of improving the experience, and (Chrome
    > eventually mitigated the problem by ignoring empty handlers)."
    
      https://developer.chrome.com/blog/update-install-criteria

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { register } from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
@@ -15,5 +14,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-register();
