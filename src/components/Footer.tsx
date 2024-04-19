84a39d0f0d8677eef79f39a4dde037e69cc1f6e5
Duncan Stuart <dgmstuart@gmail.com>
2024-04-19 02:59:05 +0200

    WIP start moving strings to translation file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+import "../i18n";
+import { useTranslation } from "react-i18next";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 
 const Footer: React.FC<{ className?: string }> = ({ className }) => {
+  const { t } = useTranslation();
+
   return (
     <footer className={classNames("Footer", className)}>
       <ul>
         <li>
-          <Link to="word_list">Full word list</Link>
+          <Link to="word_list">{t("footer.wordList")}</Link>
         </li>
         <li>
           <a
             href="https://www.youtube.com/playlist?list=PLgsIo5h4KQoYzMCvcuBFTy7-qW8VgsFpT"
             className="external"
           >
-            Full video list
+            {t("footer.videoList")}
           </a>
         </li>
         <li>
@@ -23,7 +27,7 @@ const Footer: React.FC<{ className?: string }> = ({ className }) => {
             href="https://dgmstuart.github.io/blog/2022/02/18/building-a-bingo-app-in-react/"
             className="external"
           >
-            About
+            {t("footer.about")}
           </a>
         </li>
       </ul>
