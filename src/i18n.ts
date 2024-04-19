8d852b21d7aa2b7438e740a4d122dc4cc8486bff
Duncan Stuart <dgmstuart@gmail.com>
2024-04-19 03:25:31 +0200

    Add French and Swedish translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
+import fr from "./locales/fr.json";
+import sv from "./locales/sv.json";
 
 i18n.use(initReactI18next).init({
-  resources: { en },
-  lng: "en",
+  resources: { en, fr, sv },
+  lng: "sv",
   fallbackLng: "en",
 });
