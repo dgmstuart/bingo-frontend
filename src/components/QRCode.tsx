77fee927e63b46c16bc49bab2d541948b1969d47
Duncan Stuart <dgmstuart@gmail.com>
2024-04-21 22:14:57 +0200

    Generate QR code using a library
    Remove the ugly Bit.ly logo and take a step towards making the URL
    configurable.

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import qrCode from "../images/qrCode.png";
+import QRCodeImage from "react-qr-code";
 import "./QRCode.css";
 
 const QRCode: React.FC = () => {
   return (
-    <img
-      src={qrCode}
-      className="QRCode"
-      alt="QR code for Team Lindy Bingo card"
-    />
+    <div className="QRCode">
+      <QRCodeImage
+        value="https://bit.ly/lindybingocard"
+        size={500}
+        className="QRCodeImage"
+        bgColor="hsla(28, 41%, 95%, 0.7)"
+        fgColor="hsla(232, 20%, 1%, 0.8)"
+      />
+    </div>
   );
 };
 
