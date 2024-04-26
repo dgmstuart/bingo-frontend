f99e3f770539383e0bc331b68361a3e1e3957ec9
Duncan Stuart <dgmstuart@gmail.com>
2024-04-26 21:01:51 +0200

    Add Knee slap and config typecheck

---
+++ src/data/config.typecheck.ts
@@ -0,0 +1,8 @@
+import teamLindy from "./teamLindy.json";
+import numbers from "./numbers.json";
+import balboa from "./balboa.json";
+import test from "./test.json";
+import { Config } from "./config";
+
+// Check that all config files have the expected type:
+export const configs: Config[] = [teamLindy, numbers, balboa, test];
